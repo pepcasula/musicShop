b81d86df624c231129cd2e9c3aa07ddced6a395a
Johrespi <johrespi@espol.edu.ec>
2024-01-11 01:05:13 -0500

    MAVEN PROJECT
    creating maven project in netbeans

---
+++ src/main/java/ec/edu/espol/musicshop/MusicShop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.musicshop;
+
+/**
+ *
+ * @author johan
+ */
+public class MusicShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
